e795e245a5886f39841bb5140cd3fc8cb8a905f8
Pavel Narzyaev <pavel@narzyaev.ru>
2021-06-12 17:15:08 +0300

    resolved commands circular dependency

--- src/mvc/controller/Commands.ts
+++ src/mvc/controller/Commands.ts
@@ -1,13 +1,18 @@
-import MyFacade from "../../MyFacade";
 import RandomColorProxy from "../model/RandomColorProxy";
+import {IFacade} from "../../com/koreez/puremvc/IFacade";
+
+let _facade:IFacade;
+
+export function initCommands(facade:IFacade):void {
+    _facade = facade;
+}
 
 export function startupCommand(multitonKey: string, notificationName: string) {
     console.log("execute startup command");
 }
 
 export function randomColorCommand(multitonKey: string, notificationName: string) {
-    let facade = MyFacade.getInstance();
-    let randomColorProxy:RandomColorProxy = facade.retrieveProxy(RandomColorProxy.NAME);
+    let randomColorProxy:RandomColorProxy = _facade.retrieveProxy(RandomColorProxy.NAME);
     console.log("execute random color command");
     randomColorProxy.refreshAndSendNotification();
 }
